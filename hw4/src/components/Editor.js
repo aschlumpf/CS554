6ea247d3bd1cb97428046bb8f13796fe2b6e50b7
aschlumpf <aschlumpf3412@gmail.com>
2019-10-17 17:36:44 -0400

    fix accessibility

--- hw4/src/components/Editor.js
+++ hw4/src/components/Editor.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Editor extends React.Component {
   render() {
     return (
-      <textarea id='editor' onChange={this.props.handleOnChange}></textarea>
+      <textarea aria-label="Enter markdown here" id='editor' onChange={this.props.handleOnChange}></textarea>
     );
   }
 }
